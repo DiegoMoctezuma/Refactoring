8d49781e354113dda0b620f3829d6225ab577017
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-05-01 13:50:52 -0500

    Agregando test/fizzbuzzService

---
+++ test/services/fizzbuzzService.test.js
@@ -0,0 +1,10 @@
+const FizzbuzzService = require("../../lib/services/fizzbuzzService");
+
+describe("Prueba FizzBuzzService", () => {
+  test("Añadiendo FizzBuzz a los explorers", () => {
+    const explorer1 = {name: "Explorer1", score: 15};
+    FizzbuzzService.applyValidationInExplorer(explorer1);
+
+    expect(explorer1).toEqual(expect.objectContaining({trick: "FIZZBUZZ"}));
+  });
+});
