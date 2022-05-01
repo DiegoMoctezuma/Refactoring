8df81cba876aa39cc7e0cc3f172637a8029a6f88
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-05-01 12:49:35 -0500

    Agregando carpeta test/utils

---
+++ test/utils/reader.test.js
@@ -0,0 +1,9 @@
+const Reader = require("../../lib/utils/reader");
+
+describe("Return readJsonFile", () => {
+  test("Reader", () => {
+    const explorers = Reader.readJsonFile("explorers.json");
+
+    expect(explorers[0].name).toBe("Woopa1");
+  });
+});
