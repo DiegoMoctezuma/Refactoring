089f234bbff9676fc2f5299b9b251239efe4fd3e
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-05-01 23:50:59 -0500

    Prueba de FizzBuzz con un numero

--- test/controllers/explorerController.test.js
+++ test/controllers/explorerController.test.js
@@ -2,7 +2,7 @@
 const ExplorerController = require("../../lib/controllers/explorerController");
 
 describe("Pruebas de ExplorerController", () => {
-    test("1. Obteniendo Explorers po Misión", () => {
+    test("1. Obteniendo Explorers por Misión", () => {
         const prueba1 = ExplorerController.getExplorersByMission("node");
         expect(prueba1[8].mission).toBe("node");
     });
@@ -13,5 +13,18 @@ describe("Pruebas de ExplorerController", () => {
     test("3. Obteniendo numero de Explorers por misión", () => {
         const prueba3 = ExplorerController.getExplorersAmonutByMission("node");
         expect(prueba3).toBe(10);
+    });
+    test("4. Obteniendo fizzbuzz de un número", () => {
+        const numero1 = ExplorerController.applyValidationInNumber(15);
+        expect(numero1).toBe("FIZZBUZZ");
+
+        const numero2 = ExplorerController.applyValidationInNumber(10);
+        expect(numero2).toBe("BUZZ");
+
+        const numero3 = ExplorerController.applyValidationInNumber(9);
+        expect(numero3).toBe("FIZZ");
+
+        const numero4 = ExplorerController.applyValidationInNumber(1);
+        expect(numero4).toBe(1);
     });
 });
