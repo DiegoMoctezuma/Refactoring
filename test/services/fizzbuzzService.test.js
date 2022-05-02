c2b7c836b2f2505cfc6a5a36df7da7325c55e9b5
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-05-01 21:46:06 -0500

    Test FizzBuzz con numero

--- test/services/fizzbuzzService.test.js
+++ test/services/fizzbuzzService.test.js
@@ -1,10 +1,21 @@
+/* eslint-disable no-undef */
 const FizzbuzzService = require("../../lib/services/fizzbuzzService");
 
 describe("Prueba FizzBuzzService", () => {
-    test("Añadiendo FizzBuzz a los explorers", () => {
+    test("1. Añadiendo FizzBuzz a los explorers", () => {
         const explorer1 = {name: "Explorer1", score: 15};
-        FizzbuzzService.applyValidationInExplorer(explorer1);
+        const explorerFizzBuzz = FizzbuzzService.applyValidationInExplorer(explorer1);
 
-        expect(explorer1).toEqual(expect.objectContaining({trick: "FIZZBUZZ"}));
+        expect(explorerFizzBuzz).toEqual(expect.objectContaining({trick: "FIZZBUZZ"}));
+    });
+    test("2. Probando FizzBuzz con un número", () => {
+        const number1 = FizzbuzzService.applyValidationInNumber(15);
+        expect(number1).toBe("FIZZBUZZ");
+
+        const number2 = FizzbuzzService.applyValidationInNumber(5);
+        expect(number2).toBe("BUZZ");
+
+        const number3 = FizzbuzzService.applyValidationInNumber(3);
+        expect(number3).toBe("FIZZ");
     });
 });
